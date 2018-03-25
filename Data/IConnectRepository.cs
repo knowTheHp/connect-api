d5dfa1782e5238ed77c5c35cc350ef1adfe7b6af
Harpreet Singh <harpreetsingh14388@live.com>
2018-03-25 16:36:56 +0530

    GetMainPhoto method added in the repo

--- Data/IConnectRepository.cs
+++ Data/IConnectRepository.cs
@@ -13,5 +13,6 @@ namespace connect_api.Data
         Task<IEnumerable<User>> GetUsers();
         Task<User> GetUser(int id);
         Task<Photo> GetPhoto(int id);
+        Task<Photo> GetMainPhoto(int userId);
     }
 }
