ade02156796f2671974b41da9810a2a896ce3da1
Harpreet Singh <harpreetsingh14388@live.com>
2018-03-25 20:46:40 +0530

    minor changes

--- Data/ConnectRepository.cs
+++ Data/ConnectRepository.cs
@@ -11,8 +11,8 @@ namespace connect_api.Data
     {
         private readonly DataContext _context;
         public ConnectRepository(DataContext context) => this._context = context;
-        public void Add<T>(T entity) where T : class => _context.Add(entity);
-        public void Delete<T>(T entity) where T : class => _context.Remove(entity);
+        public void Add<T>(T entity) where T : class => this._context.Add(entity);
+        public void Delete<T>(T entity) where T : class => this._context.Remove(entity);
         public async Task<User> GetUser(int id)
         {
             var user = await this._context.User.Include(p => p.Photos)
