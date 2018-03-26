be3909e4c265c642c4154faaa8f37cd3df3d46e7
Harpreet Singh <harpreetsingh14388@live.com>
2018-03-26 11:53:15 +0530

    New properties added

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace ConnectApi.Dtos
@@ -14,7 +15,19 @@ namespace ConnectApi.Dtos
         [EmailAddress]
         public string Email { get; set; }
         [Required]
+        public string Gender { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
         [StringLength(20, MinimumLength = 8, ErrorMessage = "password should be minimum of 8 characters")]
         public string Password { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
