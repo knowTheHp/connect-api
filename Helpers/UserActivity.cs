20b2224e28f930b1e4728233ebced5bc2a92e676
Harpreet Singh <harpreetsingh14388@live.com>
2018-03-26 14:10:39 +0530

    a new class added

---
+++ Helpers/UserActivity.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using connect_api.Data;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace connect_api.Helpers
+{
+    public class UserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var actionExecutedContext = await next();
+            var userId = int.Parse(actionExecutedContext.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            var connectRepo = actionExecutedContext.HttpContext.RequestServices.GetService<ConnectRepository>();
+            var user = await connectRepo.GetUser(userId);
+            user.LastActive = DateTime.Now;
+            await connectRepo.SaveAll();
+        }
+    }
+}
